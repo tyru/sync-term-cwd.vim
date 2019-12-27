1e2bc7924abed86f40509ae56a8dc20638953957
tyru <tyru.exe@gmail.com>
2019-12-27 23:52:11 +0900

    fix: don't overwrite cd command

--- macros/synctermcwd.sh
+++ macros/synctermcwd.sh
@@ -1,6 +1,6 @@
-cd() {
-  builtin cd "$@" || return $?
+_synctermcwd_ps1() {
   if [ "$VIM_TERMINAL" ]; then
     printf '\e]51;["call","Tapi_SyncTermCwd","%s"]\x07' "$PWD"
   fi
 }
+PS1="$PS1\$(_synctermcwd_ps1)"
